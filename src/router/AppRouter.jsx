cc91398f663e1c23be4d7fedbefa7a9e1d4837e8
Ana <amarcosestevez@gmail.com>
2022-08-31 12:33:24 +0200

    refactory paths

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,11 +1,14 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
-import { LoginPage } from '../auth/pages/LoginPage';
-import { MarvelPage } from '../heroes/pages/MarvelPage';
-import { DcPage } from '../heroes/pages/DcPage';
+
+import { LoginPage } from '../auth';
+import { MarvelPage, DcPage } from '../heroes';
+import { Navbar } from '../ui';
+
 
 export const AppRouter = () => {
   return (
   <>
+  <Navbar/>
      <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="marvel" element={<MarvelPage />} />
