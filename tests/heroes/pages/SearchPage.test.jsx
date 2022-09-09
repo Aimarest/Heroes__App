2ed48a735f6b5eabd9a11ebbadc954a086a7d171
Ana <amarcosestevez@gmail.com>
2022-09-09 18:26:12 +0200

    add the SearchPage task solved

--- tests/heroes/pages/SearchPage.test.jsx
+++ tests/heroes/pages/SearchPage.test.jsx
@@ -1,8 +1,18 @@
 import { fireEvent, render, screen }  from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
+import { MemoryRouter, useNavigate } from 'react-router-dom';
 import { SearchPage } from '../../../src/heroes/pages';
 
+
+const mockedUseNavigate = jest.fn();
+
+jest.mock('react-router-dom', () =>({
+    ...jest.requireActual('react-router-dom'),
+    useNavigate: () => mockedUseNavigate
+}))
+
+
 describe('Pruebas sobre el componente SearchPage', () => {
+    beforeEach(() => jest.clearAllMocks())
 
     test('Debe de mostrarse correctamente con valores por defecto', () => {
 
@@ -41,6 +51,19 @@ describe('Pruebas sobre el componente SearchPage', () => {
         const button = screen.getByRole('button');
            fireEvent.click(button);
            expect(screen.getByText('No hero with')).toBeTruthy()
+         //  screen.debug()
+      })
+      test('Debe de llamar el navigate a la pantalla nueva', () => {
+        render(
+            <MemoryRouter initialEntries={['/search']}>
+                <SearchPage />
+            </MemoryRouter>
+        );
+       const input = screen.getByRole('textbox');
+        const button = screen.getByRole('button');
+        fireEvent.change(input,{target: { value:'superman' }});
+           fireEvent.click(button);
+         expect(mockedUseNavigate).toHaveBeenCalledWith(`?q=superman`)
            screen.debug()
       })
-})
+       })
