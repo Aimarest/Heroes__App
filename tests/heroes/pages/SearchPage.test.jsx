ad1b423b2a13a9cca813e4b2fa3e5d06e7bfaa64
Ana <amarcosestevez@gmail.com>
2022-09-09 18:05:01 +0200

    add the task at searchPage solved

--- tests/heroes/pages/SearchPage.test.jsx
+++ tests/heroes/pages/SearchPage.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen }  from '@testing-library/react';
+import { fireEvent, render, screen }  from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import { SearchPage } from '../../../src/heroes/pages';
 
@@ -13,4 +13,34 @@ describe('Pruebas sobre el componente SearchPage', () => {
         );
         expect( container ).toMatchSnapshot();
     })
+
+    test('Debe de mostrar a Batman y el input con el valor del queryString', () => {
+
+         render(
+             <MemoryRouter initialEntries={['/search?q=batman']}>
+                 <SearchPage />
+             </MemoryRouter>
+         );
+        
+         const button = screen.getByRole('button');
+            const input = screen.getByRole('textbox');
+         fireEvent.click(button);
+         expect(input.value).toBe('batman');
+         const img = screen.getByRole('img');
+         expect(img.src).toContain( "/assets/heroes/dc-batman.jpg"); 
+      
+     })
+
+     test('Debe mostrar un error si no se encuentra el héroe buscado (batman123)', () => { 
+        render(
+            <MemoryRouter initialEntries={['/search?q=batman123']}>
+                <SearchPage />
+            </MemoryRouter>
+        );
+       
+        const button = screen.getByRole('button');
+           fireEvent.click(button);
+           expect(screen.getByText('No hero with')).toBeTruthy()
+           screen.debug()
+      })
 })
